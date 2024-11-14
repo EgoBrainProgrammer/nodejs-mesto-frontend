1aef36133f93152a2cf530054e8c70840391c44b
unknown <predator_x_x_x@mail.ru>
2024-11-15 09:28:31 +1100

    Update

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+require('dotenv').config({
+  path: path.resolve(process.cwd(), '.env.deploy'),
+});
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master',
+} = process.env;
+
+module.exports = {
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: 'https://github.com/yandex-praktikum/nodejs-mesto-frontend.git',
+      path: DEPLOY_PATH,
+      'post-deploy': 'npm i && npm run build',
+    },
+  },
+};
